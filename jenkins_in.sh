6a079de78f7ec8059436fc70ddde65b45f92395b
Gauri Tambe. <154447205+gauritambe@users.noreply.github.com>
2026-01-09 21:11:02 +0530

    Create jenkins_in.sh

---
+++ jenkins_in.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+java -version -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y 
+sudo apt install jenkins -y
+
+#Add user
+sudo usermod -aG jenkins ubuntu
