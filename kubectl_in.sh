4d9438c890b71a0c14b1d78d6352046089c2e56a
Gauri Tambe. <154447205+gauritambe@users.noreply.github.com>
2026-01-13 21:50:09 +0530

    Update kubectl_in.sh

--- kubectl_in.sh
+++ kubectl_in.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# Script to install kubectl on an instance
 
-# Update package list
-sudo apt update -y
-
-# Install curl
-sudo apt install curl -y
-
-# Download the latest stable kubectl binary
-curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-
-# Install kubectl and set correct permissions
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Verify the installation
 kubectl version --client
